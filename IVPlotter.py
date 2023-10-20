a4f2e35305084b924f4ab27ad11750491335fa85
chiappe666 <jfchiappeb@gmail.com>
2023-10-20 03:40:44 -0300

    Added list for selecting data to plot, and table visualization

--- IVPlotter.py
+++ IVPlotter.py
@@ -1,4 +1,7 @@
-from dash import Dash, html, dcc, callback, Output, Input, State  # type: ignore
+import dash
+import dash_bootstrap_components as dbc
+from dash import Dash, html, dcc, dash_table, callback, Output, Input, State
+from ipywidgets.widgets.trait_types import date_from_json  # type: ignore
 import plotly.express as px  # type: ignore
 import plotly.graph_objects as go  # type: ignore
 from plotly.subplots import make_subplots  # type: ignore
@@ -7,61 +10,161 @@ import base64
 import io
 import plotly.io as pio  # type: ignore
 import names  # type: ignore
+import json
+import re
 
 pd.options.plotting.backend = "plotly"
-
+external_stylesheets = "https://codepen.io/chriddyp/pen/bWLwgP.css"
 # Initialize the app
-app = Dash(__name__)
-
-external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
-
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__)
 
 app.layout = html.Div(
     [
-        html.Label("Enter Plot Name:"),
-        dcc.Input(id="plot-name", type="text", value="My Plot"),
+        html.Div(
+            "Enter Plot Name:", style={"marginLeft": "10px", "marginRight": "0px"}
+        ),
+        dbc.Input(
+            id="plot-name", type="text", value="My Plot", style={"margin": "0 auto"}
+        ),
+        dbc.Button("Plot", id="btn_plot"),
         dcc.Upload(
             id="upload-data",
             children=html.Div(["Drag and Drop or ", html.A("Select Files")]),
             style={
-                "width": "100%",
+                "width": "90%",
                 "height": "60px",
                 "lineHeight": "60px",
                 "borderWidth": "1px",
                 "borderStyle": "dashed",
                 "borderRadius": "5px",
                 "textAlign": "center",
-                "margin": "10px",
+                "margin": "0 auto",
             },
             # Allow multiple files to be uploaded
             multiple=True,
         ),
-        dcc.Checklist(
+        dbc.Checklist(
             id="file-checklist",
+            style={"padding": "10px"},
         ),
-        html.Div(id="output-data-upload"),
-        html.Button("Download as png", id="btn_png"),
+        dbc.Row(
+            [
+                dbc.Col(
+                    id="output-data-upload",
+                    width=8,
+                ),
+                dbc.Col(
+                        dash_table.DataTable(
+                            id="Table",
+                            style_data={
+                                "whiteSpace": "normal",
+                            },
+                            style_cell={
+                                "whiteSpace": "normal",
+                                "font-size": "0.6rem",
+                                "overflowX": "scroll",
+                                'minWidth': '0px', 'maxWidth': '180px',
+                            },
+                            style_table={
+                                "overflowX": "scroll",
+                            },
+                            fixed_columns={"headers": True, "data": 1},
+                        ),
+                    width=4,
+                ),
+            ],
+        ),
+        dbc.Button("Download as png", id="btn_png"),
+        dbc.Button("Download as html", id="btn_html"),
         dcc.Download(id="download-plot-png"),
+        dcc.Download(id="download-plot-html"),
         dcc.Store(id="figure-store", storage_type="memory"),
-    ]
+        dcc.Store(id="dataframe-store", storage_type="memory"),
+    ],
 )
 
 
+import pandas as pd
+
 class IVdata:
+    """
+    A class to represent IV data.
+
+    Attributes
+    ----------
+    data : pandas.DataFrame
+        The IV data.
+    metadata : pandas.DataFrame
+        The metadata associated with the IV data.
+
+    Methods
+    -------
+    get_data()
+        Returns the IV data.
+    get_metadata()
+        Returns the metadata associated with the IV data.
+    add(IVdat)
+        Adds another IVdata object to the current object.
+    to_json()
+        Returns the IV data and metadata as a list of dictionaries.
+    """
+
     def __init__(self, df1=pd.DataFrame(), df2=pd.DataFrame()):
+        """
+        Parameters
+        ----------
+        df1 : pandas.DataFrame, optional
+            The IV data, by default an empty DataFrame.
+        df2 : pandas.DataFrame, optional
+            The metadata associated with the IV data, by default an empty DataFrame.
+        """
         self.data = df1
         self.metadata = df2
 
     def get_data(self):
+        """
+        Returns
+        -------
+        pandas.DataFrame
+            The IV data.
+        """
         return self.data
 
     def get_metadata(self):
+        """
+        Returns
+        -------
+        pandas.DataFrame
+            The metadata associated with the IV data.
+        """
         return self.metadata
 
     def add(self, IVdat):
-        self.data = pd.concat([self.data, IVdat.data])
-        self.metadata = pd.concat([self.metadata, IVdat.metadata])
+        """
+        Adds another IVdata object to the current object.
+
+        Parameters
+        ----------
+        IVdat : IVdata
+            The IVdata object to add.
+        """
+        self.data = pd.concat([self.data, IVdat.data], ignore_index=True)
+        self.metadata = pd.concat(
+            [self.metadata, IVdat.metadata], ignore_index=True)
+
+    def to_json(self):
+        """
+        Returns
+        -------
+        list
+            The IV data and metadata as a list of dictionaries.
+        """
+        data = self.data
+        datadict = data.to_dict()
+        metadata = self.metadata
+        metadatadict = metadata.to_dict()
+        list = [datadict, metadatadict]
+        return list
 
 
 def to_float(s):
@@ -72,12 +175,28 @@ def to_float(s):
 
 
 def Hier(df):
-    # Add a new level to the index using groupby and cumcount
-    df.set_index([df.groupby(level=0).cumcount(), df.index], append=True, inplace=True)
+    """
+    Adds a new level to the index of a pandas DataFrame using groupby and cumcount, and then resets the index to keep only two levels.
 
-    # Reset the index to keep only two levels
+    Args:
+        df (pandas.DataFrame): The DataFrame to modify.
+
+    Returns:
+        None
+    """
+    df.set_index([df.groupby(level=0).cumcount(), df.index],
+                 append=True, inplace=True)
+
     df.reset_index(level=[2], drop=True, inplace=True)
     return
+
+
+def json_to_IVdata(j):
+    j1 = j[0]
+    j2 = j[1]
+    data = pd.DataFrame.from_dict(j1)
+    metadata = pd.DataFrame.from_dict(j2)
+    return IVdata(data, metadata)
 
 
 def Readivdat(rawdata, mname):
@@ -90,6 +209,15 @@ def Readivdat(rawdata, mname):
     DUTID = ""
     data_label = ""
     for line in rawdata:
+        if "Date" in line:
+            # Define a regular expression pattern to match the date portion
+            pattern = r'Date:\s+"(.+)"'
+            # Use re.search to find the date portion
+            match = re.search(pattern, line)
+            if match:
+                date = match.group(1)  # Get the matched date portion
+            else:
+                date = "Date not found."
         if "END FOOTER" in line:
             break
         if "END DATA" in line:
@@ -109,7 +237,8 @@ def Readivdat(rawdata, mname):
             source_index = line.index("Source(") + len("Source(")
             output_index = line.index("Output(%):", source_index)
             end_index = line.index("/I(A):", output_index)
-            input_power = float(line[output_index + len("Output(%):") : end_index])
+            input_power = float(
+                line[output_index + len("Output(%):"): end_index])
             input_power_value = str(input_power) + "%"
         if FOOT:
             mdata = line.split(":")
@@ -121,17 +250,28 @@ def Readivdat(rawdata, mname):
         return None
     df_metadata = pd.DataFrame([metadata], columns=mlabel)
     df_metadata["InputPower"] = input_power_value  # Add InputPower column
-    df_metadata["Name"] = mname  # Add Measurement Name
+    df_metadata["Name"] = mname + " | " + date  # Add Measurement Name
     df_metadata["id"] = id
     df_metadata["DUTID"] = DUTID
     df = pd.DataFrame(data, columns=data_label)
-    df["Name"] = mname  # Add Measurement Name
+    df["Name"] = mname + " | " + date  # Add Measurement Name
     df["id"] = id
     DF = IVdata(df, df_metadata)
     return DF
 
 
 def PlotIV(DF, title):
+    """
+    Plots current and power vs voltage for multiple measurements with different input powers.
+
+    Args:
+    - DF: a pandas DataFrame containing the data to plot and metadata about the measurements.
+    - title: a string with the title of the plot.
+
+    Returns:
+    - fig: a plotly figure object with the current and power vs voltage plot.
+    """
+
     # Create figure with secondary y-axis
     fig = make_subplots(specs=[[{"secondary_y": True}]])
 
@@ -194,17 +334,18 @@ def PlotIV(DF, title):
         legend=dict(itemclick="toggle"),
         title={
             "text": title,
-            #'y':0.9,
+            # 'y':0.9,
             "x": 0.5,
             "xanchor": "center",
             "yanchor": "top",
             "font": dict(size=50),
         },
     )
+
     return fig
 
 
-def Process_Files(list_of_contents, list_of_names, plotname):
+def Process_Files(list_of_contents, list_of_names):
     d = IVdata()
     for content, mname in zip(list_of_contents, list_of_names):
         _, content_string = content.split(",")
@@ -214,25 +355,51 @@ def Process_Files(list_of_contents, list_of_names, plotname):
         data.data["Current(A)"] *= -1
         data.data["Power(W)"] *= -1
         d.add(data)
-    d.data.set_index("id", inplace=True)
-    Hier(d.data)
-    fig = PlotIV(d, plotname)
-    return html.Div([dcc.Graph(figure=fig)]), fig
+    return d
+
+
+@callback(
+    Output("dataframe-store", "data"),
+    Output("file-checklist", "options"),
+    Input("upload-data", "contents"),
+    State("upload-data", "filename"),
+    prevent_initial_call=True,
+)
+def update_output(list_of_contents, list_of_names):
+    if list_of_contents is not None:
+        DF = Process_Files(list_of_contents, list_of_names)
+    A = DF.to_json()
+    result = DF.data[["id", "Name"]].drop_duplicates()
+    list_of_dicts = [
+        {"label": row["Name"], "value": row["id"]} for _, row in result.iterrows()
+    ]
+    return A, list_of_dicts
 
 
 @callback(
     Output("output-data-upload", "children"),
     Output("figure-store", "data"),
-    Input("upload-data", "contents"),
-    State("upload-data", "filename"),
-    Input("plot-name", "value"),
+    Output("Table", "columns"),
+    Output("Table", "data"),
+    # Input("btn_plot", "n_clicks"),
+    Input("file-checklist", "value"),
+    State("plot-name", "value"),
+    State("dataframe-store", "data"),
+    prevent_initial_call=True,
 )
-def update_output(list_of_contents, list_of_names, plotname):
-    fig = None
-    if list_of_contents is not None:
-        children, fig = Process_Files(list_of_contents, list_of_names, plotname)
-        return children, fig.to_json()
-    return None, None
+def plot(value, plotname, dataframe_data):
+    if value is not None:
+        DF = json_to_IVdata(dataframe_data)
+        DF.data = DF.data[DF.data["id"].isin(value)]
+        DF.metadata = DF.metadata[DF.metadata["id"].isin(value)]
+        DF.data.set_index("id", inplace=True)
+        Hier(DF.data)
+        fig = PlotIV(DF, plotname)
+        DF.metadata = DF.metadata.set_index("Name").T.reset_index()
+        # DF.metadata.index.name = None
+        columns = [{"name": i, "id": i} for i in DF.metadata.columns]
+        data = DF.metadata.to_dict("records")
+        return html.Div([dcc.Graph(figure=fig)]), fig, columns, data
 
 
 @callback(
@@ -241,9 +408,10 @@ def update_output(list_of_contents, list_of_names, plotname):
     State("plot-name", "value"),
     State("figure-store", "data"),
     prevent_initial_call=True,
+    allow_duplicates=True
 )
 def export_png(n_clicks, plotname, fig_data):
-    fig = pio.from_json(fig_data)
+    fig = go.Figure(fig_data)
     if n_clicks is not None:
         # Generate the PNG image of the figure
         fig_bytes = fig.to_image(format="png")
@@ -259,6 +427,26 @@ def export_png(n_clicks, plotname, fig_data):
         return data
     return None
 
+@callback(
+    Output("download-plot-html", "data"),
+    Input("btn_html", "n_clicks"),
+    State("plot-name", "value"),
+    State("figure-store", "data"),
+    prevent_initial_call=True,
+    allow_duplicates=True
+)
+def export_html(n_clicks, plotname, fig_data):
+    fig = go.Figure(fig_data)
+    if n_clicks is not None:
+        # Generate the HTML file of the figure
+        html_bytes = fig.to_html()
 
+        # Prepare the data for download
+        data = dict(
+            content=html_bytes, filename=f"{plotname}.html", base64=False, type="html"
+        )
+
+        return data
+    return None
 if __name__ == "__main__":
     app.run(debug=True)
